b6148c4e9e5c58a19be2db50f0ed8e11c0a71ef6
Ilya <87614970+Do960D@users.noreply.github.com>
2021-08-19 16:25:47 +0300

    + comment about  char value

--- choiceMENU.cpp
+++ choiceMENU.cpp
@@ -7,14 +7,14 @@ int main()
 
 	std::cin >> check;
 
-	a = '1';
+	a = '1'; // single quotes are required. specificity of char
 	b = '2';
 	c = '3';
 
 	do
 	{
 
-		if (check == a) { std::cout << "a"; goto choice; }
+		if (check == a) { std::cout << "a"; goto choice; } 
 
 		if (check == b) { std::cout << "b"; goto choice; }
 
