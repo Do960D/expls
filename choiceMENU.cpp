1e99ca0bd38fe9b88fa97fb5460068f3455a77eb
Шипарев Илья Валерьевич <i.shiparev@VP.local>
2021-08-19 16:22:45 +0300

    the menu for selecting a part of a code to run

---
+++ choiceMENU.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+int main() 
+{
+	char check, a, b, c;
+	
+	choice:
+
+	std::cin >> check;
+
+	a = '1';
+	b = '2';
+	c = '3';
+
+	do
+	{
+
+		if (check == a) { std::cout << "a"; goto choice; }
+
+		if (check == b) { std::cout << "b"; goto choice; }
+
+		if (check == c) { std::cout << "c"; goto choice; }
+
+		else { std::cout << "no"; }
+
+	}
+
+		while (check == 0);
+	
+	
+	return 0; 
+}
