9a6ec4618acb7f16d1372bfe8e5e9bd36bc91c2d
Шипарев Илья Валерьевич <i.shiparev@VP.local>
2021-08-19 18:02:50 +0300

    random and compare with users input

---
+++ iknowurmind.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+
+int main()
+{
+	srand ( static_cast <size_t> (time(0)) ) ;
+	size_t secret, tries, aiGuess ;
+
+	std::cout << "Enter a number, and don`t tell it to any body!\n\n" ;
+	std::cin >> secret ;
+	tries = 0;
+
+	do
+	{
+		aiGuess = rand();
+		tries++;
+	}
+
+	while (aiGuess != secret) ;
+
+	std::cout << "AI got your mind! Just in " << tries << " tries!" ;
+
+	return 0 ;
+}
+
