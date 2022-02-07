b82fa77756abb42412c292e9e42c79a5ea80368b
Ilya <87614970+Do960D@users.noreply.github.com>
2022-02-07 11:56:16 +0300

    Add files via upload

---
+++ zeroFinder.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+	srand(time(NULL));
+
+	std::string text;
+
+	int lineCounter = 0;
+	int* ptrLineCounter = &lineCounter;
+	
+	for (size_t i = 0; i < rand(); i++)
+	{
+		int min = 97;
+		int max = 122;
+		
+		bool luck = rand() % 10;
+
+		char letter = min + (rand() % ((max - min) + 1));
+
+		text += letter;
+				
+		if (!luck)
+		{
+			text += "\n";
+			++ *ptrLineCounter;
+
+		}
+	}
+
+	std::cout << text << std::endl << *ptrLineCounter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
