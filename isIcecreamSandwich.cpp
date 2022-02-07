e31664ec30a55fe3642034bd428bb33945a2825c
Ilya <87614970+Do960D@users.noreply.github.com>
2022-02-07 14:12:05 +0300

    task: string with same ends and different center.
    https://ravesli.com/praktika-chast-1/#toc-0

---
+++ isIcecreamSandwich.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	
+	std::string isIcecreamSandwich;
+	//std::cin >> isIcecreamSandwitch;
+
+	isIcecreamSandwich = "aaccaa";	
+
+	if (isIcecreamSandwich.size() <= 2)
+	std::cout << "too less symbols, bude. write more.";
+	
+	int j = isIcecreamSandwich.size();
+
+	for (std::string::size_type i = 0; i < isIcecreamSandwich.size(); ++i)
+	{	
+		if (isIcecreamSandwich[i] == (isIcecreamSandwich[j-1]))
+		{
+			std::cout << "Sandwitch structure is good\n";
+			--j;
+		}
+	
+		else
+		{
+			std::cout << "it is not a sandwich\n";
+			break;
+		}
+	}
+
+}
+
+
